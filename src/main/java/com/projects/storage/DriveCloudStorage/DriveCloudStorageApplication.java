a6f6794f6642d3628f44b4c48e42345334abb45e
Aleksander Żulewski <azulewski@gmail.com>
2020-07-05 20:56:29 +0200

    First commit

---
+++ src/main/java/com/projects/storage/DriveCloudStorage/DriveCloudStorageApplication.java
@@ -0,0 +1,13 @@
+package com.projects.storage.DriveCloudStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveCloudStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveCloudStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/storage/DriveCloudStorage/DriveCloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.storage.DriveCloudStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveCloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
